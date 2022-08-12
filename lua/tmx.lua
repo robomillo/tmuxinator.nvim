fdd482884e0fcc29a074fd56bef28adf28573c86
robomill <84311571+robomill@users.noreply.github.com>
2022-08-12 10:24:27 +0200

    switch to telescope

--- lua/tmx.lua
+++ lua/tmx.lua
@@ -67,81 +67,6 @@ function G:increment_access_count(project_name)
   )
 end
 
-local function opt_len()
-  local count = 0
-  for _ in pairs(opt_map) do count = count + 1 end
-  return count
-end
-
-local function get_longest_opt()
-  local sorted = {}
-  for _, v in pairs(opt_map) do
-    table.insert(sorted, v)
-  end
-  table.sort(sorted, function(a, b) return #a < #b end)
-  return sorted[1]
-end
-
-local function create_initial_float()
-  G.base_win_height = vim.api.nvim_win_get_height(G.base_win)
-  G.base_win_width = vim.api.nvim_win_get_width(G.base_win)
-  local width = string.len(get_longest_opt()) + 20
-  local height = 10
-  local opts = {
-    relative = 'editor',
-    row = height,
-    col = math.floor(G.base_win_width * 0.5) - width,
-    width = width,
-    height = height,
-    style = "minimal",
-    border = "rounded"
-  }
-  G.tmx_buf = vim.api.nvim_create_buf(true, true)
-  G.tmx_win = vim.api.nvim_open_win(G.tmx_buf, true, opts)
-  vim.api.nvim_win_set_option(G.tmx_win, "cursorline", true)
-end
-
-local function center(win, str)
-  local width = vim.api.nvim_win_get_width(win)
-  local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
-  local left_and_center = string.rep(' ', shift) .. str
-  return left_and_center .. string.rep(' ', width - string.len(left_and_center))
-end
-
-local function set_options_in_win()
-  -- set favourite highlight group
-  vim.api.nvim_set_hl(0, "favourite", { underline = true, bg = "#24283b", fg = "#e0af68" })
-
-  -- center content
-  local content = {}
-  for k, v in pairs(opt_map) do
-    table.insert(content, center(G.tmx_win, v))
-  end
-  -- set content
-  vim.api.nvim_buf_set_lines(G.tmx_buf, 0, -1, false, content)
-  local i = 0
-  for k, v in ipairs(opt_map) do
-    if favourites[k] ~= nil then
-      vim.api.nvim_buf_add_highlight(G.tmx_buf, -1, "favourite", i, 0, -1)
-    end
-    i = i + 1
-  end
-  -- register command on buffer to process selection
-  vim.api.nvim_buf_set_keymap(G.tmx_buf, 'i', '<Esc>',
-    "<C-c>:q<CR>",
-    { noremap = true })
-  vim.api.nvim_buf_set_keymap(G.tmx_buf, 'n', '<Esc>',
-    ":q<CR>",
-    { noremap = true })
-  vim.api.nvim_buf_set_keymap(G.tmx_buf, 'n', '<CR>',
-    "<cmd>lua require('tmx').opt_selected()<CR>",
-    { noremap = true })
-
-  vim.api.nvim_buf_set_keymap(G.tmx_buf, 'n', '<leader>f',
-    "<cmd>lua require('tmx').toggle_favourite()<CR>",
-    { noremap = true })
-end
-
 local function switch_session()
   print("Switching to: " .. G.selected)
   Job:new({
@@ -190,7 +115,7 @@ end
 
 local function toggle_favourite()
   local selection = action_state.get_selected_entry()
-  G.selected = selection[1]
+  G.selected = selection.value
   G:toggle_favourite(G.selected)
   get_and_set_sorted_options()
 end
@@ -200,21 +125,37 @@ local function highlight_favourite(entry)
   return entry
 end
 
+local function contains(t, value)
+  for _, v in ipairs(t) do
+    if value == v then
+      return true
+    end
+  end
+  return false
+end
+
 local function tmx()
   get_and_insert_new()
   get_and_set_sorted_options()
+  vim.api.nvim_set_hl(0, "Fav", { underline = true, fg = "#e0af68" })
 
   local find = function(opts)
     opts = opts or {}
     pickers.new(opts, {
-      prompt_title = "colors",
+      prompt_title = "tmx",
       finder = finders.new_table {
         results = opt_map,
         entry_maker = function(entry)
           return {
             value = entry,
-            display = highlight_favourite(entry),
-            ordinal = entry,
+            display = function(ent)
+              if contains(favourites, ent.value) then
+                return "* " .. ent.value .. " *", { { { 0, 100 }, "Fav" } }
+              else
+                return ent.value
+              end
+            end,
+            ordinal = entry
           }
         end
       },
@@ -224,7 +165,7 @@ local function tmx()
         actions.select_default:replace(function()
           actions.close(prompt_bufnr)
           local selection = action_state.get_selected_entry()
-          G.selected = selection[1]
+          G.selected = selection.value
           opt_selected()
         end)
         return true
@@ -232,12 +173,8 @@ local function tmx()
     }):find()
   end
 
-  -- to execute the function
   find(require("telescope.themes").get_dropdown {})
-  -- G.original_buf = vim.api.nvim_get_current_buf()
-  -- G.base_win = vim.api.nvim_get_current_win()
-  -- create_initial_float()
-  -- set_options_in_win()
+
 end
 
 local function setup()
